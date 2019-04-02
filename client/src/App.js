cc05c52c9d36d6b1b760915419fc25c7e5b42e3b
jonmagneson <jonmagneson@gmail.com>
2019-04-02 13:31:54 -0600

    created landing page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
+import Landing from './components/Landing'
+import NoMatch from './components/NoMatch'
 import FetchUser from './components/FetchUser'
 import { Container, } from 'semantic-ui-react';
 import { Route, Switch, } from 'react-router-dom';
@@ -13,9 +15,11 @@ const App = () => (
       <FetchUser>
         <Container>
           <Switch>
-           <Route exact path="/" component={Home} />
+           <Route exact path="/" component={Landing} />
+           <Route exact path="/home" component={Home} />
            <Route exact path="/login" component={Login} />
            <Route exact path="/register" component={Register} />
+           <Route component={NoMatch} />
           </Switch>
           </Container>
       </FetchUser>
