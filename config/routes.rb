9674c38e2693f96810ddcce0f7f120d4a1150576
Jake Bills <jakebills1@gmail.com>
2019-04-02 10:57:24 -0600

    added quiz model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :quizzes
+  end
+
 end
---
+++ db/migrate/20190402161417_create_quizzes.rb
@@ -0,0 +1,9 @@
+class CreateQuizzes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :quizzes do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
