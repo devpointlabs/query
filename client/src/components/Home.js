6a48d2c7650a7b4605fd283d14a7957f41d0c35c
Jake Bills <jakebills1@gmail.com>
2019-04-02 17:20:59 -0600

    look for teacher landing page; added to seeds.rb

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,26 @@
 import React from  'react';
 import { AuthConsumer, } from '../providers/AuthProvider'
 import { Button } from 'semantic-ui-react';
-const Home = (props) => (
-  <AuthConsumer>
-    { auth => (
-      <div>{auth.authenticated ? 
-      <div>
-        <h1>Welcome</h1> 
-        <Button onClick={() => auth.handleLogout(props.history)}>Logout</Button>
-      </div>
-      : 
-      <h1>Please login or create an account</h1>
-      }</div>
-    )}
+const Home = (props) => {
+  document.body.style = 'background: #6D55A3;'
+
+  return (
+    <AuthConsumer>
+      { auth => (
+        <div>{auth.authenticated ? 
+          <div>
+            <h1>Welcome, {auth.user.name}</h1> 
+            {auth.user.teacher ? <Button inverted>Create a Quiz</Button> : <Button inverted>See Your Quizzes</Button>}
+            <Button onClick={() => auth.handleLogout(props.history)} inverted>Logout</Button>
+          </div>
+          : 
+          <h1>Please login or create an account</h1>
+          }
+        </div>
+      )}
 
 
-  </AuthConsumer>
-)
+    </AuthConsumer>
+  )
+}
 export default Home;
