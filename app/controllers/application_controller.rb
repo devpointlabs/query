1cd4d9acc24ae738e3cead34809fef7c70c139ec
Jake Bills <jakebills1@gmail.com>
2019-04-03 16:53:06 -0600

    fix bug in questions_controller, nomatch, set dependent destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :configure_permitted_parameters, if: :devise_controller?
+  include DeviseTokenAuth::Concerns::SetUserByToken
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :teacher])
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :quiz
-  has_many :choices
+  has_many :choices, dependent: :destroy
 end
