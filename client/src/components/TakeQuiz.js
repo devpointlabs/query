11a913e0920bb62f36940d26fdf8f3b91ab7ac0f
Luke Herrmann <herrmannluke@gmail.com>
2019-04-11 11:16:32 -0600

    Split types of questions into their own components

--- client/src/components/TakeQuiz.js
+++ client/src/components/TakeQuiz.js
@@ -1,46 +1,67 @@
 import React from 'react';
 import axios from 'axios';
 import Timer from './Timer'
-import {Grid, } from "semantic-ui-react"
+import { Grid, } from "semantic-ui-react"
 import { AuthConsumer, } from "../providers/AuthProvider"
 import styled from "styled-components"
+import MC from "../quiz_components/MC"
+import Open from "../quiz_components/Open"
+import TorF from "../quiz_components/TorF"
 
 
 
 class TakeQuiz extends React.Component {
-    state = { quiz: {}, questions: [], choices: [], student_choice: "", }
-    componentDidMount() {
-        axios.get(`/api/quizzes/${this.props.match.params.id}`)
-        .then(res => {
-            this.setState({ quiz: res.data })
-        });
-    }
-    render() {
-       
+    state = { quiz: {}, questions: [], choices: [], questionsIds: [], student_choice: "", }
 
-        const { name, info } = this.state.quiz
-        
+    componentDidMount() {
+        const quiz_id = this.props.match.params.id
+        axios.get(`/api/quizzes/${quiz_id}`)
+            .then(res => {
+                this.setState({ quiz: res.data })
+            })
+        axios.get(`/api/quizzes/${quiz_id}/questions`)
+            .then(res1 => {
+                for (let ques of res1.data) {
+                    let ques_id = ques.id
+                    axios.get(`/api/questions/${ques_id}/choices`)
+                        .then(res => {
+                            this.setState({ questions: [{ ...ques, choices: res.data }, ...this.state.questions] })
+                        })
+                }
+            })
+    }
+
+
+    render() {
+        const quiz_name = this.state.quiz.name
+        const quiz_info = this.state.quiz.info
+
         return (
-        <Grid divided='vertically'>
-          <DescContainer>
-              <div >
-                <HeaderText fSize="medium">{name}</HeaderText>
-                <HeaderText>{info}</HeaderText>
-                <HeaderText fSize="small">Submission is <strong>Anonymous</strong></HeaderText>
-                <HeaderText fSize="tiny">The creator of this query won't know</HeaderText>
-                <HeaderText fSize="tiny">which submission belongs to you.</HeaderText>
-                <HeaderText fSize="tiny">Time Remaining:</HeaderText>
-                <HeaderText fSize="tiny">5 minutes</HeaderText>
-              </div>
-            </DescContainer>
-            <QuesContainer>
-                <p>Hello</p>
-                <p>Hello</p>
-                <p>Hello</p>
-                <p>Hello</p>
-                <p>Hello</p>
-            </QuesContainer>
-        </Grid>
+            <Grid divided='vertically'>
+                <DescContainer>
+                    <div >
+                        <HeaderText fSize="medium">{quiz_name}</HeaderText>
+                        <HeaderText>{quiz_info}</HeaderText>
+                        <HeaderText fSize="small">Submission is <strong>Anonymous</strong></HeaderText>
+                        <HeaderText fSize="tiny">The creator of this query won't know</HeaderText>
+                        <HeaderText fSize="tiny">which submission belongs to you.</HeaderText>
+                        <HeaderText fSize="tiny">Time Remaining:</HeaderText>
+                        <HeaderText fSize="tiny">5 minutes</HeaderText>
+                    </div>
+                </DescContainer>
+                <QuesContainer>
+                    {/* Depending on the question type it will render a component that formats the question */}
+                    {this.state.questions.map(question => {
+                        if (question.qType === "MC") {
+                            return <MC question={question.name} choices={question.choices} />
+                        } else if (question.qType === "open") {
+                            return <Open question={question.name} />
+                        } else if (question.qType === "TorF") {
+                            return <TorF question={question.name} choices={question.choices} />
+                        }
+                    })}
+                </QuesContainer>
+            </Grid>
         )
     }
 }
@@ -58,31 +79,53 @@ width: 40%
 
 const QuesContainer = styled.div`
 display: flex;
-justify-content: center;
-align-items: center;
+// justify-content: center;
+// align-items: center;
 flex-direction: column;
 height: 100vh;
 width: 60%;
 `
 
 const HeaderText = styled.h1`
-  color: white !important;
-  font-size: ${props => fontSize(props.fSize)} !important;
+color: white !important;
+font-size: ${props => fontSize(props.fSize)} !important;
 `
 const fontSize = (size) => {
-    switch(size) {
-    case 'large':
-        return '4rem';
-    case 'medium':
-        return '2rem';
-    case 'small':
-        return '1.5rem';
-    case 'tiny':
-        return '.75rem';
-    default:
-        return '1rem';
+    switch (size) {
+        case 'large':
+            return '4rem';
+        case 'medium':
+            return '2rem';
+        case 'small':
+            return '1.5rem';
+        case 'tiny':
+            return '.75rem';
+        default:
+            return '1rem';
     }
-  }
+}
+
+const QuizList = styled.ul`
+    `
+
 
 
 export default TakeQuiz
+
+
+
+        // <ListItem key={question.id}>
+        //     <strong>
+        //         {question.qType}
+        //     </strong>
+        //     <QuizList>
+        //         {question.choices.map(choice => {
+        //             debugger
+        //             return (
+        //                 <ChoiceItem>
+        //                     {choice.answer}
+        //                 </ChoiceItem>
+        //             )
+        //         })}
+        //     </QuizList>
+        // </ListItem>
---
+++ client/src/styles/TakeQuizStyles.js
@@ -0,0 +1,53 @@
+
+
+const DescContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+background: #6D55A3;
+height: 100vh;
+width: 40%
+`
+
+const QuesContainer = styled.div`
+display: flex;
+// justify-content: center;
+// align-items: center;
+flex-direction: column;
+height: 100vh;
+width: 60%;
+`
+
+const HeaderText = styled.h1`
+color: white !important;
+font-size: ${props => fontSize(props.fSize)} !important;
+`
+const fontSize = (size) => {
+    switch (size) {
+        case 'large':
+            return '4rem';
+        case 'medium':
+            return '2rem';
+        case 'small':
+            return '1.5rem';
+        case 'tiny':
+            return '.75rem';
+        default:
+            return '1rem';
+    }
+}
+
+const QuizList = styled.ul`
+    `
+const ListItem = styled.li`
+    padding: 10px;
+    font-size: 1.5rem;
+    margin: 0 0 20px 0;
+    list-style-type: none;
+    `
+const ChoiceItem = styled.li`
+    margin: 10px;
+    font-size: 1rem;
+    list-style-type: none;
+    `
