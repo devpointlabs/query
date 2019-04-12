86b9b75e3cb120a7005c28cf57b14ec287db8f1a
Jake Bills <jakebills1@gmail.com>
2019-04-12 14:45:37 -0600

    stuff

--- client/src/components/DynamicMCForm.js
+++ client/src/components/DynamicMCForm.js
@@ -38,6 +38,7 @@ class DynamicMCForm extends React.Component {
     this.props.toggleForm()
     this.props.toggleButtons()
   }
+  
   render() {
     const { name, explanation, choices, addAChoice } =  this.state
     return (
