e904ef94f40537c258b4e22f1200eda223bbe563
Jake Bills <jakebills1@gmail.com>
2019-04-16 15:54:29 -0600

    refactoring to submissions by quiz

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Profile from './components/Profile'
 import ShowQuiz from './components/ShowQuiz'
 import TakeQuiz from './components/TakeQuiz'
 import ResultsByQuiz from './components/results/ResultsByQuiz'
+import Results from './components/results/Results'
 import { Container, } from 'semantic-ui-react';
 import { Route, Switch, } from 'react-router-dom';
 import ProtectedRoute from "./components/ProtectedRoute"
@@ -23,6 +24,7 @@ const App = () => (
            <Route exact path="/login" component={Login} />
            <ProtectedRoute exact path="/profile" component={Profile} />
            <ProtectedRoute exact path="/resultsbyquiz" component={ResultsByQuiz} />
+           <ProtectedRoute exact path="/results" component={Results} />
            <Route exact path="/quizzes/:id/quiz" component={TakeQuiz} />
            <Route exact path="/register" component={Register} />
            <Route exact path="/register/teacher" component={Register} />
--- client/src/components/results/ResultsByQuiz.js
+++ client/src/components/results/ResultsByQuiz.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect} from 'react'
 import axios from 'axios';
+import { Card } from 'semantic-ui-react';
+import Navbar from '../Navbar';
+import { Link, } from 'react-router-dom'
 
 const ResultsByQuiz = () => {
   const [quizzes, setQuizzes] = useState([]);
@@ -11,8 +14,23 @@ const ResultsByQuiz = () => {
       })
       .catch( err => console.log(err))
   }, [])
+  document.body.style = 'background: #5906A3;'
   return (
-    <div></div>
+    <>
+      <Navbar />
+      <Card.Group centered>
+        {quizzes.map( q => (
+          <Card key={q.id}>
+            <Card.Content>
+              <Card.Header>{q.name}</Card.Header>
+            </Card.Content>
+            <Card.Content>
+              <Link to="/results">See Results</Link>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
+    </>
   )
 }
 
