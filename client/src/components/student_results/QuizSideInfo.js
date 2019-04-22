74becf94fce6a5a8522ab41e2c42fc46445afc91
bweegee <bw33g33@gmail.com>
2019-04-22 10:53:13 -0600

    Navbar added to QuizSideInfo

--- client/src/components/student_results/QuizSideInfo.js
+++ client/src/components/student_results/QuizSideInfo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, } from "react";
 import axios from 'axios'
 import styled from 'styled-components'
+import Navbar from '../Navbar'
 
 const QuizSideInfo = ({ quiz_id }) => {
   const [quiz, setQuiz] = useState([])
@@ -13,6 +14,8 @@ const QuizSideInfo = ({ quiz_id }) => {
   }, []);
   
   return (
+    <div>
+    <Navbar />
     <SideDiv> 
       <BigHead>
         {quiz.name}
@@ -21,24 +24,24 @@ const QuizSideInfo = ({ quiz_id }) => {
         {quiz.info}
       </MedHead>
       <hr />
-      {/* {anon ? (
-        <Header as="h4" style={textStyle}>
+      {quiz.anon ? (
+        <MedHead>
           Submission is <strong>Anonymous</strong>
-        </Header>
+        </MedHead>
       ) : (
-        <Header as="h4" style={textStyle}>
+        <MedHead>
           Submission is <strong>Identified</strong>
-        </Header>
+        </MedHead>
       )}
-      {anon ? (
-        <Header as="p" style={textStyle}>
+      {quiz.anon ? (
+        <SmallHead>
           The creator of this query won't know who you are.
-        </Header>
+        </SmallHead>
       ) : (
-        <Header as="p" style={textStyle}>
+        <SmallHead>
           The creator of this query will know who you are.
-        </Header>
-      )} */}
+        </SmallHead>
+      )}
       <SmallHead>
         Time Remaining:
         5 minutes
@@ -46,17 +49,16 @@ const QuizSideInfo = ({ quiz_id }) => {
       <MedHead>
         Submitted at:
       <SmallHead>
-        {/* tuesday March 8, etc */}
         04/20/69
       </SmallHead>
       </MedHead>
     </SideDiv> 
+  </div>
   );
 };
 
 const SideDiv = styled.div`
   color: white;
-  text-align: center;
   justify-content: center;
   letter-spacing: .2rem;
   padding: 45px;
@@ -81,16 +83,9 @@ const MedHead = styled.div`
 
 const SmallHead = styled.h4`
   font-family: menlo;
-  font-weight: normal;
+  font-size: .9em;
+  font-style: italic;
   padding: 10px;
   margin: 5px;
 `
-
-const textStyle = {
-  color: "white",
-  textAlign: "center",
-  fontFamily: 'menlo',
-  // fontStyle: 'oblique'
-};
-
 export default QuizSideInfo;
